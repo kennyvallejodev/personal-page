66854a8d9e83e1fc6aa50ed832fcf341b0b4692b
kennyvallejodev <keenyy1997@gmail.com>
2022-02-09 23:25:47 +0100

    add pdf to the website and also redirect /cv to the pdf

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
-const withImages = require('next-images');
+const withImages = require("next-images");
+const { getRedirectStatus } = require("next/dist/lib/load-custom-routes");
 
 module.exports = withImages({
   reactStrictMode: true,
-	images: {
-		domains: ['kenny-page-assets.s3.us-west-2.amazonaws.com']
-	},
-	webpack(config, options) {
-		return config;
-	},
+  images: {
+    domains: ["kenny-page-assets.s3.us-west-2.amazonaws.com"],
+  },
+  webpack(config, options) {
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/cv",
+        destination: "/cv.pdf",
+        permanent: true,
+      },
+    ];
+  },
 });
